afdc9173a95f1c1d540dd18fde77a1f4f5bc1c42
Chosen-David <yuanshuowang37@gmail.com>
2025-08-17 08:29:46 +0800

    加入了HALF8

--- Elementwise/elementwise.cu
+++ Elementwise/elementwise.cu
@@ -14,6 +14,7 @@
 
 #define FLOAT4(v)=*reinterpret_cast<float4 *>(&v)//也可以用[0]解引用
 #define HALF2(v)=*reinterpret_cast<half2 *>(&v)
+#define HALF8(v)=*reinterpret_cast<float4 *>(&v)
 
 // ElementWise Add + Vec4
 // grid(N/256), block(256/4)
@@ -44,4 +45,21 @@ __global__ void add_vec2_f16(half* a,half* b,half* c,int N){
         HALF2(c[idx])=reg_c;
     }
 }
+
+//可以进一步向量化访存，float4 = 16B = half8 (注意没有half8 但是你有float4)
+__global__ void add_vec8_f16(half* a,half* b,half* c,int N){
+    int idx=8*(threadIdx.x+blockDim.x*blockIdx.x);
+    half pack_a[8],pack_b[8],pack_c[8];
+    HALF8(pack_a[0])=HALF8(a[idx]);//注意本质是取pack_a[0]的地址然后连续放8个half
+    HALF8(pack_b[0])=HALF8(b[idx]);//注意HALF8是给地址用的
+    for(int i=0;i<8;i+=2){
+        HALF2(pack_c[i])=__hadd2(HALF2(pack_a[i]),HALF2(pack_b[i]));//注意HALF2可以重新解释为half2的值
+    }
+    if((idx+7)<N){
+        HALF8(c)=HALF8(pack_c[idx]);
+    }
+
+
+}
+
 
